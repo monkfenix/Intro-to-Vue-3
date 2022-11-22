ab198675b9852a7707e2fa95c1bb18ff7163ae81
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-11-22 18:50:44 +1000

    completed lesson 11 challenge

---
+++ components/ReviewList.js
@@ -0,0 +1,26 @@
+app.component('review-list', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        },
+    },
+    template:
+        /*html*/
+        `
+    <div class="review-container">
+        <h3>Reviews: </h3>
+        <ul>
+            <li v-for="(review, index) in reviews" :key="index">
+            {{review.name}} gave this {{review.rating}} stars
+            <br />
+            <p>
+                {{review.name}} 
+                <span v-if="review.recommend">recommended this!<span>
+                <span v-else> did not recommend this</span>
+            </p>
+            "{{review.review}}"</li>
+        </ul>
+    </div>
+    `
+})
