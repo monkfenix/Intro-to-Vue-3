3d6fc0f28100352c4a523a01ff71dc37debcb2ba
Nissan Dookeran <nissan.dookeran@gmail.com>
2022-11-22 14:16:22 +1000

    finish challenge for lesson 8

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
+            onSale: true,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -19,6 +19,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        subTitle() {
+            return this.onSale ? `${this.title} is on Sale!` : ''
         }
     }
 })
